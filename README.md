6169af6d2d93a4b43ee5ce66c2da7a3131affcc8
Vipul Swarup <vipulswarup@Vipuls-MacBook-Air-M1.local>
2023-08-28 09:31:50 +0530

    Fixed config etc to make the app launch

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Document Anonymizer
+
+## Installation
+
+First install Streamlit (of course!) then pip-install this library:
+
+```bash
+pip install streamlit
+pip install st-annotated-text
+```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "
+[server]\n
+headless = true\n
+enableCORS=false\n
+port = 80\n
 " > ~/.streamlit/config.toml
